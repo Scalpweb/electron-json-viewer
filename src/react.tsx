e8522901b6b39d55d2996b33312b10eaa82f86c9
Pascal Scalpweb Mugnier <scalpweb@gmail.com>
2020-04-13 22:42:12 +0200

    Add json file opening

--- src/react.tsx
+++ src/react.tsx
@@ -1,14 +1,30 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import ReactJson from "react-json-view";
+import { readFileSync } from "fs";
+
+const remote = require("electron").remote;
 
 const Index = () => {
+  const [text, setText] = React.useState("{}");
   const [json, setJson] = React.useState({});
   const update = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     try {
+      setText(event.target.value);
       setJson(JSON.parse(event.target.value));
     } catch (e) {}
   };
+
+  React.useEffect(() => {
+    const last: string = remote.process.argv[remote.process.argv.length - 1];
+    if (last.endsWith(".json")) {
+      try {
+        const data = readFileSync(last, "utf-8");
+        setJson(JSON.parse(data));
+        setText(data);
+      } catch {}
+    }
+  }, []);
 
   return (
     <>
@@ -22,6 +38,7 @@ const Index = () => {
             padding: "5px",
           }}
           onChange={update}
+          value={text}
         />
       </div>
       <div style={{ width: "50%", float: "left", height: "100%" }}>
